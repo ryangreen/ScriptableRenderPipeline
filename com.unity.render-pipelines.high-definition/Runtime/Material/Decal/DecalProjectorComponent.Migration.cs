cf80d40c691eddadcd7b6fd1770aecdcdaf45e8e
RSlysz <remi@unity3d.com>
2019-03-07 14:35:46 +0100

    Add missing scale independance migration and offset and size swap axis

--- com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalProjectorComponent.Migration.cs
+++ com.unity.render-pipelines.high-definition/Runtime/Material/Decal/DecalProjectorComponent.Migration.cs
@@ -10,18 +10,38 @@ namespace UnityEngine.Experimental.Rendering.HDPipeline
         enum Version
         {
             Initial,
-            UseZProjectionAxis
+            UseZProjectionAxisAndScaleIndependance
         }
 
         static readonly MigrationDescription<Version, DecalProjectorComponent> k_Migration = MigrationDescription.New(
-            MigrationStep.New(Version.UseZProjectionAxis, (DecalProjectorComponent decal) =>
+            MigrationStep.New(Version.UseZProjectionAxisAndScaleIndependance, (DecalProjectorComponent decal) =>
             {
-                // rotate so projection move from -Y to Z but childs keep same positions and rotations
-                decal.transform.Rotate(new Vector3(90, 0, 0));
-                foreach(Transform child in decal.transform)
+                // Update size for scale independence
+                decal.m_Size.Scale(decal.transform.lossyScale);
+
+                //Rotate so projection move from - Y to Z but childs keep same positions and rotations
+                decal.transform.RotateAround(decal.transform.position, decal.transform.right, 90);
+                foreach (Transform child in decal.transform)
                 {
                     child.RotateAround(decal.transform.position, decal.transform.right, -90f);
                 }
+
+                // Invese Y and Z in the size so we keep same aspect
+                float newZ = decal.m_Size.y;
+                decal.m_Size.y = decal.m_Size.z;
+                decal.m_Size.z = newZ;
+
+                // Invese Y and Z in the offset so we keep same aspect and take into account new Scale independence
+                newZ = -decal.m_Offset.y * decal.transform.lossyScale.y / decal.transform.localScale.y;
+                decal.m_Offset.y = decal.m_Offset.z * decal.transform.lossyScale.z / decal.transform.localScale.z;
+                decal.m_Offset.z = newZ;
+                decal.m_Offset.x *= decal.transform.lossyScale.x / decal.transform.localScale.x;
+
+                // Update decal system
+                if (decal.m_Handle != null)
+                    DecalSystem.instance.RemoveDecal(decal.m_Handle);
+                Matrix4x4 sizeOffset = Matrix4x4.Translate(decal.offset) * Matrix4x4.Scale(decal.size);
+                decal.m_Handle = DecalSystem.instance.AddDecal(decal.position, decal.rotation, Vector3.one, sizeOffset, decal.m_DrawDistance, decal.m_FadeScale, decal.uvScaleBias, decal.m_AffectsTransparency, decal.m_Material, decal.gameObject.layer, decal.m_FadeFactor);
             })
         );
 
